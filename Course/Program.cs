74fde2ceffe7a431350ee5f33ce1dca466bf109c
Djalma <djalma.ant@gmail.com>
2020-05-02 18:53:10 -0300

    Using a Simple Method reference as parameter

--- Course/Program.cs
+++ Course/Program.cs
@@ -14,12 +14,17 @@ namespace Course
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Table", 450.00));
 
-            list.Sort(); // O Sort só funciona porque a Classe 'Product' implementa a Interface 'IComparable'
+            list.Sort(CompareProducts); // Usando o Método Auxiliar 'CompareProducts' como Argumento do Método 'Sort'
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()); // Criando um método auxiliar para usá-lo como Argumento do Método 'Sort'
         }
     }
 }
