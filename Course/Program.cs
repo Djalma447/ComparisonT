6b9b7e057e2eaae37cf617d6123dd16fde9ad55b
Djalma <djalma.ant@gmail.com>
2020-05-02 19:13:02 -0300

    Lambda Expression to a Delegate variable

--- Course/Program.cs
+++ Course/Program.cs
@@ -14,19 +14,13 @@ namespace Course
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Table", 450.00));
 
-            Comparison<Product> comp = CompareProducts; // Variável do Tipo Delegate tendo uma referência de Método atribuído a ela (A Variável Delegate)
-            
-            list.Sort(comp); // Usando a Variável do Tipo Delegate como referência [ao Método Auxiliar 'CompareProducts'] como Argumento do Método 'Sort'
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()); // Expressão Lambda (OU Expressão Anônima) atribuída a uma variável do Tipo Delegate
+            list.Sort(comp); // Usando a Variável do Tipo Delegate como referência [A Expressão Lambda] como Argumento do Método 'Sort'
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
-        }
-
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()); // Criando um método auxiliar para usá-lo como Argumento do Método 'Sort'
         }
     }
 }
