d4017cb38bbb2c6094c41853df4b38527ea5aab9
Djalma <djalma.ant@gmail.com>
2020-05-02 18:26:11 -0300

    Using Interface IComparable

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
 
 namespace Course
 {
@@ -6,7 +8,18 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Table", 450.00));
+
+            list.Sort(); // O Sort só funciona porque a Classe 'Product' implementa a Interface 'IComparable'
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
         }
     }
 }
