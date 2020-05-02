183b00c7f4000e3fcb27434a60d95dbd0190a6af
Djalma <djalma.ant@gmail.com>
2020-05-02 19:17:25 -0300

    Inline Lambda Expression

--- Course/Program.cs
+++ Course/Program.cs
@@ -14,8 +14,7 @@ namespace Course
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Table", 450.00));
 
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()); // Expressão Lambda (OU Expressão Anônima) atribuída a uma variável do Tipo Delegate
-            list.Sort(comp); // Usando a Variável do Tipo Delegate como referência [A Expressão Lambda] como Argumento do Método 'Sort'
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); // Usando a A Expressão Lambda diretamente como Argumento do Método 'Sort'
 
             foreach (Product p in list)
             {
